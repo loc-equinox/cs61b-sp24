beb87d301e75e829109995f0294fba123cc7570b
Leshan Chen <2400013135@stu.pku.edu.cn>
2025-01-24 11:18:19 +0800

    complete hw0b-MapExercises

--- hw0b/src/MapExercises.java
+++ hw0b/src/MapExercises.java
@@ -1,26 +1,44 @@
+import org.antlr.v4.runtime.tree.Tree;
+
 import java.util.List;
 import java.util.Map;
+import java.util.TreeMap;
 
 public class MapExercises {
     /** Returns a map from every lower case letter to the number corresponding to that letter, where 'a' is
      * 1, 'b' is 2, 'c' is 3, ..., 'z' is 26.
      */
     public static Map<Character, Integer> letterToNum() {
-        // TODO: Fill in this function.
-        return null;
+        Map<Character, Integer> map = new TreeMap<>();
+        char ch = 'a';
+        for (int i = 1; i <= 26; i++) {
+            map.put(ch, i);
+            ch ++;
+        }
+        return map;
     }
 
     /** Returns a map from the integers in the list to their squares. For example, if the input list
      *  is [1, 3, 6, 7], the returned map goes from 1 to 1, 3 to 9, 6 to 36, and 7 to 49.
      */
     public static Map<Integer, Integer> squares(List<Integer> nums) {
-        // TODO: Fill in this function.
-        return null;
+        Map<Integer, Integer> map = new TreeMap<>();
+        for (int i : nums) {
+            map.put(i, i * i);
+        }
+        return map;
     }
 
     /** Returns a map of the counts of all words that appear in a list of words. */
     public static Map<String, Integer> countWords(List<String> words) {
-        // TODO: Fill in this function.
-        return null;
+        Map<String, Integer> map = new TreeMap<>();
+        for (String w : words) {
+            if (map.containsKey(w)) continue;
+            int cnt = 0;
+            for (String match : words)
+                if (match.equals(w)) cnt++;
+            map.put(w, cnt);
+        }
+        return map;
     }
 }
